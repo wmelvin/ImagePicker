4947ac15684833a575e0cd87afebc2b054482ce6
Bill Melvin <bill@billmelvin.com>
2023-11-29 17:09:36 -0500

    Add to About dialog

--- ImagePicker/uaboutdlg.pas
+++ ImagePicker/uaboutdlg.pas
@@ -13,7 +13,9 @@ type
 
   TAboutDlg = class(TForm)
     btnClose: TButton;
+    imgLogo: TImage;
     lblLazarusUrl: TLabel;
+    lblFpcUrl: TLabel;
     lblSourceUrl: TLabel;
     lblSource: TLabel;
     lblAppName: TLabel;
@@ -21,7 +23,9 @@ type
     lblLaz: TLabel;
     Panel1: TPanel;
     procedure FormShow(Sender: TObject);
+    procedure lblFpcUrlClick(Sender: TObject);
     procedure lblLazarusUrlClick(Sender: TObject);
+    procedure lblLazClick(Sender: TObject);
     procedure lblSourceUrlClick(Sender: TObject);
   private
 
@@ -47,11 +51,22 @@ begin
   lblAPpVersion.Caption := 'Version ' + APP_VERSION;
   lblSourceUrl.Caption := 'https://github.com/wmelvin/ImagePicker';
   lblLazarusUrl.Caption := 'https://www.lazarus-ide.org';
+  lblFpcUrl.Caption := 'https://www.freepascal.org';
+end;
+
+procedure TAboutDlg.lblFpcUrlClick(Sender: TObject);
+begin
+  OpenURL(lblFpcUrl.Caption);
 end;
 
 procedure TAboutDlg.lblLazarusUrlClick(Sender: TObject);
 begin
   OpenURL(lblLazarusUrl.Caption);
+end;
+
+procedure TAboutDlg.lblLazClick(Sender: TObject);
+begin
+
 end;
 
 procedure TAboutDlg.lblSourceUrlClick(Sender: TObject);
